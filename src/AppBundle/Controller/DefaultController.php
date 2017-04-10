619ee2607d80d536c5f3dd96d4f3aed38f9e7193
Jahir Lozano <jahirlozano@gmail.com>
2017-04-10 18:30:29 +0200

    CRUD Ejemplo Habitacion

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Habitacion;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
@@ -18,14 +20,105 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+      
+    /**
+     * @Route("/listar", name="listar")
+     */
+    public function listarAction()
+    {
+    	
+    	$repository = $this->getDoctrine()->getRepository('AppBundle:Habitacion');
+    	$habitaciones = $repository->findAll();	
+    	return $this->render('AppBundle:Default:listar_habitaciones.html.twig', array('habitaciones' => $habitaciones));
+    	
+    }
     
     /**
-     * @Route("/prueba", name="prueba")
+     * @Route("/crear", name="crearGet")
+     * @Method({"GET"})
      */
-    public function pruebaAction()
+    public function formCrearAction()
     {
-    	// replace this example code with whatever you need
-    	return $this->render('AppBundle:Default:prueba.html.twig');
+    	
+    	return $this->render('AppBundle:Default:crear_habitacion.html.twig');
+    	
     }
+    
+    
+    /**
+     * @Route("/crear", name="crearPost")
+     * @Method({"POST"})
+     */
+    public function crearAction(Request $request)
+    {
+ 		$nombre=$request->request->get('nombre');
+ 		$descripcion=$request->request->get('descripcion');
+ 		
+    	$habitacion = new Habitacion();
+    	$habitacion->setNombre($nombre);
+    	$habitacion->setDescripcion($descripcion);
+    	
+    	$em = $this->getDoctrine()->getManager();
+    	$em->persist($habitacion);
+    	$em->flush();
+    	$this->get('session')->getFlashBag()->set('succesfull', 'Habitacion Creada');
+    	return $this->redirectToRoute('listar');
+    	
+    }
+    
+    /**
+     * @Route("/editarGet/{id}", name="editarGet")
+     * @Method({"GET"})
+     */
+    public function editarGetAction($id)
+    {
+    	
+    	$repository = $this->getDoctrine()->getRepository('AppBundle:Habitacion');
+    	$habitacion = $repository->find($id);	
+    	return $this->render('AppBundle:Default:habitacion.html.twig', array('habitacion' => $habitacion));
+    	
+    	
+    }
+    
+    /**
+     * @Route("/editarPost/{id}", name="editarPost")
+     * @Method({"POST"})
+     */
+    public function editarPostAction($id,Request $request)
+    {
+    	$nombre=$request->request->get('nombre');
+    	$descripcion=$request->request->get('descripcion');
+    	
+    	$repository = $this->getDoctrine()->getRepository('AppBundle:Habitacion');
+    	$habitacion = $repository->find($id);	
+
+    	$habitacion->setNombre($nombre);
+    	$habitacion->setDescripcion($descripcion);
+    	
+    	$em = $this->getDoctrine()->getManager();
+    	$em->persist($habitacion);
+    	$em->flush();
+    	$this->get('session')->getFlashBag()->set('succesfull', 'Habitacion Modificada');
+    	return $this->redirectToRoute('listar');
+    	
+    	
+    	
+    }
+    
+    
+    /**
+     * @Route("/borrar/{id}", name="borrar")
+     */
+    public function borrarAction($id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$repository=$em->getRepository('AppBundle:Habitacion');
+    	$habitacion = $repository->find($id);
+    	$em->remove($habitacion);
+    	$em->flush();
+    	$this->get('session')->getFlashBag()->set('succesfull', 'Habitacion borrada');
+    	return $this->redirectToRoute('listar');
+    }
+    
     
 }
---
+++ src/AppBundle/Entity/Habitacion.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Habitacion
+ *
+ * @ORM\Table(name="habitacion")
+ * @ORM\Entity
+ */
+class Habitacion
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=100, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="string", length=100, nullable=false)
+     */
+    private $descripcion;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Habitacion
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     *
+     * @return Habitacion
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+}
