e3a759e9a825fa329a97e28f94d03c0691d6852f
practicaMDW <practicaMDW@gmail.com>
2017-04-18 20:53:27 +0200

    Change name of ReservaControllerTest

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,7 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        
+        $this->assertContains('Booking', $crawler->filter('.navbar-header .navbar-brand')->text());
     }
 }
