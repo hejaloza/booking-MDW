4b0720c1947125c54740dec1f9725627cbf38c5f
practicaMDW <practicaMDW@gmail.com>
2017-04-18 19:51:55 +0200

    Listar reservas + Cancelar reserva + Tests

---
+++ tests/AppBundle/Controller/ReservaControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CadenaControllerTest extends WebTestCase
+{
+    public function testListarReservas()
+    {
+    	$client = static::createClient();
+    	$repository = $this->getDoctrine()->getRepository('AppBundle:Reserva');
+    	$reservas = $repository->findAll()->count();
+    	
+    	$crawler = $client->request('GET', '/listarReservas');
+    	
+    	$this->assertEquals(200, $cllient->getResponse()->getStatusCode());
+    	$this->assertEquals($reservas + 1, $crawler->filter('html:contains("</tr>")')->count());
+    }
+}
