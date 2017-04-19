592c59e755b800e1fb30b2f63420b7f3a29eff15
practicaMDW <practicaMDW@gmail.com>
2017-04-19 12:49:40 +0200

    Refactor Cadena Tests

--- tests/AppBundle/Controller/CadenaControllerTest.php
+++ tests/AppBundle/Controller/CadenaControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Entity\Cadena;
 
 class CadenaControllerTest extends WebTestCase
 {
@@ -10,45 +11,32 @@ class CadenaControllerTest extends WebTestCase
     {
         $client = static::createClient();
         
-        $cadenaprueba = new Cadena();
-        $cadenaprueba->setNombre("TestPruebaCadena");
-        $cadenaprueba->setResponsable("Responsable Prueba");
-        $cadenaprueba->setLogotipo("Prueba");
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($cadenaprueba);
-        $em->flush();
+        $data = array('nombre' => "TestPruebaCadena", 'responsable' => "Responsable Prueba", 'logotipo' => "Logotipo Prueba");
+        
+        $client->request('POST', '/crearCadena', $data);
         
         $crawler = $client->request('GET', '/listarCadenas');
         
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertGreaterThan(0, $crawler->filter('html:contains("TestPruebaCadena")')->count());
     }
-    
+
     public function testListarCadena()
     {
     	$client = static::createClient();
-    	$repository = $this->getDoctrine()->getRepository('AppBundle:Cadena');
-    	$cadenas = $repository->findAll()->count();
     	
     	$crawler = $client->request('GET', '/listarCadenas');
     	
-    	$this->assertEquals(200, $cllient->getResponse()->getStatusCode());
-    	$this->assertEquals($cadenas + 1, $crawler->filter('html:contains("</tr>")')->count());
+    	$this->assertEquals(200, $client->getResponse()->getStatusCode());
+    	$this->assertGreaterThan(0, $crawler->filter('html:contains("TestPruebaCadena")')->count());
     }
     
     public function testEditarCadena()
     {
     	$client = static::createClient();
-    	
-    	$repository = $this->getDoctrine()->getRepository('AppBundle:Cadena');
-    	$cadena = $repository->findByNombre("TestPruebaCadena");	
-    	
-    	$cadena->setNombre("TestPruebaCadena2");
-    	
-    	$em = $this->getDoctrine()->getManager();
-    	$em->persist($cadena);
-    	$em->flush();
+	
+    	$data = array('nombre' => "TestPruebaCadena2", 'responsable' => "Responsable Prueba", 'logotipo' => "Logotipo Prueba");
+    	$client->request('POST', '/editarCadena/1', $data);
     	
     	$crawler = $client->request('GET', '/listarCadenas');
     	
@@ -59,13 +47,9 @@ class CadenaControllerTest extends WebTestCase
     public function testBorrarCadena()
     {
     	$client = static::createClient();
-    	$repository = $this->getDoctrine()->getRepository('AppBundle:Cadena');
-    	$cadena = $repository->findByNombre("TestPruebaCadena2");
-    	$em->remove($cadena);
-    	$em->flush();
     	
+    	$client->request('GET', '/borrarCadena/1');
     	$crawler = $client->request('GET', '/listarCadenas');
-    	
     	$this->assertEquals(200, $client->getResponse()->getStatusCode());
     	$this->assertEquals(0, $crawler->filter('html:contains("TestPruebaCadena2")')->count());
     }
